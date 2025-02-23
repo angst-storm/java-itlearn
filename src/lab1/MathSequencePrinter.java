fea29736eae97f762adfd89b149a14f8f813b30b
Kiprin Sergei <sergei@jinteki.dev>
2025-02-23 18:24:29 +0500

    implemented 9 MathSequencePrinter

---
+++ src/lab1/MathSequencePrinter.java
@@ -0,0 +1,19 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class MathSequencePrinter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("N: ");
+        int n1 = in.nextInt();
+
+        int n0 = n1 - 1;
+        int n2 = n1 + 1;
+        double n3 = Math.pow(n0 + n1 + n2, 2);
+
+        System.out.printf("%s %s %s %s", n0, n1, n2, n3);
+        in.close();
+    }
+}
