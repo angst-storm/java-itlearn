788499d21016b5763d2647085a23af369f3844ec
Kiprin Sergei <sergei@jinteki.dev>
2025-02-23 17:47:49 +0500

    implemented 1 FullNamePrinter

---
+++ src/lab1/FullNamePrinter.java
@@ -0,0 +1,22 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class FullNamePrinter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Surname: ");
+        String surname = in.nextLine();
+
+        System.out.println("First Name: ");
+        String firstName = in.nextLine();
+
+        System.out.println("Patronymic: ");
+        String patronymic = in.nextLine();
+
+        System.out.println(String.join(" ", "Hello", surname, firstName, patronymic));
+        in.close();
+    }
+}
+
