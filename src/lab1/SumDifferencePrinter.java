342e7055ec8d871605baa69f843741f02f4e9cd6
Kiprin Sergei <sergei@jinteki.dev>
2025-02-23 18:26:17 +0500

    implemented 10 SumDifferencePrinter

---
+++ src/lab1/SumDifferencePrinter.java
@@ -0,0 +1,19 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class SumDifferencePrinter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Число a: ");
+        int a = in.nextInt();
+
+        System.out.println("Число b: ");
+        int b = in.nextInt();
+
+        System.out.println("Сумма: " + (a + b));
+        System.out.println("Разность: " + (a - b));
+        in.close();
+    }
+}
