e25d07a10b932b67e091c2e63bc29c122bbcffbc
Kiprin Sergei <sergei@jinteki.dev>
2025-02-23 17:55:51 +0500

    implemented 3 DatePrinter

---
+++ src/lab1/DatePrinter.java
@@ -0,0 +1,21 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class DatePrinter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("День недели: ");
+        String weekDay = in.nextLine();
+
+        System.out.println("Месяц: ");
+        String month = in.nextLine();
+
+        System.out.println("Номер дня: ");
+        String monthDay = in.nextLine();
+
+        System.out.println(String.join(", ", weekDay, monthDay, month));
+        in.close();
+    }
+}
